0caec0b5c24aa0b22d5255bfbfe8fa30c219adc4
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-05-21 11:53:28 -0500

    fix bug in signup screen # 052121

--- Unsplah_Clone/Module/MainMenu/AccountScreenModule/ViewModel/RegisterViewModel.swift
+++ Unsplah_Clone/Module/MainMenu/AccountScreenModule/ViewModel/RegisterViewModel.swift
@@ -13,22 +13,28 @@ class RegisterViewModel {
     let db = Firestore.firestore()
     
     func registerAccount(email: UITextField, password: UITextField, firstName: UITextField, lastName: UITextField, userName: UITextField, controller: UIViewController) {
-        if let email = email.text, let password = password.text {
-            Auth.auth().createUser(withEmail: email, password: password) { user, error in
-                if let error = error {
-                    print(error.localizedDescription)
-                    let alert = ReusableComponent.alertMessage(title: "Signup Error", message: "Password or ID is invalid try again")
-                    controller.present(alert, animated: true, completion: nil)
-                } else {
-                    print("Create Success")
-                    guard let firstName = firstName.text, let lastName = lastName.text, let userName = userName.text, let user = user else { fatalError("user info invalid") }
-                    self.db.collection("users").addDocument(data: ["firstName": firstName, "lastName": lastName, "userName": userName, "uid": user.user.uid]) { error in
-                        if error != nil {
-                            print("data store error")
+        if let email = email.text, let password = password.text, let firstName = firstName.text, let lastName = lastName.text, let userName = userName.text {
+            if !firstName.isEmpty && !lastName.isEmpty && !userName.isEmpty {
+                Auth.auth().createUser(withEmail: email, password: password) { user, error in
+                    if let error = error {
+                        
+                        print(error.localizedDescription)
+                        let alert = ReusableComponent.alertMessage(title: "Signup Error", message: "Signup error try again")
+                        controller.present(alert, animated: true, completion: nil)
+                    } else {
+                        print("Create Success")
+                        guard let user = user else { fatalError("no user") }
+                        self.db.collection("users").addDocument(data: ["firstName": firstName, "lastName": lastName, "userName": userName, "uid": user.user.uid]) { error in
+                            if error != nil {
+                                print("data store error")
+                            }
                         }
+                        controller.performSegue(withIdentifier: "CompleteSignUp", sender: nil)
                     }
-                    controller.performSegue(withIdentifier: "CompleteSignUp", sender: nil)
                 }
+            } else {
+                let alert = ReusableComponent.alertMessage(title: "User Information Error", message: "Information invalid")
+                controller.present(alert, animated: true, completion: nil)
             }
         }
     }
